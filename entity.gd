e5c820ca52f45d8e9d73cc4cdad2d8ed98310d15
Arysaka0953 <58541475+hsanku@users.noreply.github.com>
2020-09-21 22:44:22 +0700

    Reinventing the animation control for the character, as using anim Tree sucks

--- entity.gd
+++ entity.gd
@@ -1,28 +1,50 @@
 extends KinematicBody2D
-onready var animTree = $texture/AnimationTree
-onready var animState = animTree.get("parameters/playback")
+onready var anim = $texture/AnimationPlayer
+onready var animTree = $texture/AnimationTree 
+onready var animState = animTree.get("parameters/playback") 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 var motion = Vector2.ZERO
-var maxSpeed = 350
-var Acc = 2000
-var dcc = 15000
+export var maxSpeed = 350
+export var Acc = 300
+export var dcc = 15000
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+var last_anim = ""
 func _physics_process(delta):
 	var axis = get_input_axis()
-	animTree.set("parameters/idle/blend_position", axis)
-	animTree.set("parameters/walk/blend_position", axis)
+	print(last_anim)
+	#animTree.set("parameters/idle/blend_position", axis)
+	#animTree.set("parameters/walk/blend_position", axis)
 	if axis == Vector2.ZERO:
 		apply_friction()
-		animState.travel("idle")
+			
+		anim.play(last_anim)
+		last_anim = ""
 	else:
+		#animState.travel("walk")
+		motion = axis * Acc
 		
-		animState.travel("walk")
-		apply_movement(axis * Acc*delta)
+		if axis.x == 0 and axis.y <= -0.1 :
+			if (anim.get_current_animation().get_basename() != "walk_up"):
+				anim.play("walk_up")
+			last_anim = "idle_up"
+		elif axis.x >= 0.1 :
+			if (anim.get_current_animation().get_basename() != "walk_right"):
+				anim.play("walk_right")
+			last_anim = "idle_right"
+		elif axis.x == 0 and axis.y >= 0.1 :
+			if (anim.get_current_animation().get_basename() != "walk_down"):
+				anim.play("walk_down")
+			last_anim = "idle_down"
+		elif axis.x <= -0.1 :
+			if (anim.get_current_animation().get_basename() != "walk_left"):
+				anim.play("walk_left")
+			last_anim = "idle_left"
+
 	motion = move_and_slide(motion)
 	
 
@@ -38,7 +60,4 @@ func get_input_axis():
 #	pass
 func apply_friction():
 	motion = Vector2.ZERO
-func apply_movement(acceleration):
-	motion += acceleration
-	if motion.length() > maxSpeed :
-		motion = motion.normalized()* maxSpeed
+
